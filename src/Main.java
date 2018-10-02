04c92a79b681a2633b834dd666e582b45e8b58e6
Niklavs Meiers <niklavs@burti.lv>
2018-10-02 09:24:31 +0100

    Added .gitignore and the implementation.

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        VigenereCipher vc = new VigenereCipher();
+        vc.run();
+    }
+}
--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -1,2 +1,49 @@
-public class VigenereCipher {
+import java.util.Scanner;
+
+class VigenereCipher {
+
+    private String message;
+    private String key;
+    private String encodedMessage;
+    private Scanner userInput = new Scanner(System.in);
+
+    void run() {
+        message = "";
+        key = "";
+        encodedMessage = "";
+
+        askForMessage();
+        askForKey();
+        calculateEncodedMessage();
+        printEncodedMessage();
+        run();
+    }
+
+    private void askForMessage() {
+        System.out.println("Enter message:");
+        message = userInput.next();
+    }
+
+    private void askForKey() {
+        System.out.println("Enter key:");
+        key = userInput.next();
+    }
+
+        private void calculateEncodedMessage() {
+        StringBuilder sb = new StringBuilder();
+        int lengthOfMessage = message.length();
+        int keyIndex = 0;
+
+        for (int i = 0; i < lengthOfMessage; i++, keyIndex++) {
+            if (keyIndex >= key.length()) {
+                keyIndex = 0;
+            }
+            sb.append((char) (message.charAt(i) + (key.charAt(keyIndex) - 97)));
+        }
+        encodedMessage = sb.toString();
+    }
+
+    private void printEncodedMessage() {
+        System.out.println("Encoded message: " + encodedMessage);
+    }
 }
